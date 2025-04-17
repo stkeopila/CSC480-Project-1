32e89edeca8093b42c2f48a1d6e05b14176dd96a
stkeopila <skeopila@calpoly.edu>
2025-04-17 15:35:24 -0700

    added a file reader to find robot location start point

--- planner.py
+++ planner.py
@@ -2,16 +2,28 @@ import sys
 
 # python3 make_vacuum_world.py 5 7 0.15 3 > sample-5x7.txt
 
-def uniform_cost():
-    
+def uniform_cost(rob_loc):
+    print(rob_loc)
     return None
 
-if __name__ == '__main__':
+def find_start():
+    starting_point = None
     with open(sys.argv[2], 'r') as f:
-        columns = print(f.read(1))
-        rows = print(f.read(2))
+        columns = f.read(1)
+        rows = f.read(2)
+        for row, line in enumerate(f):
+            for col, cell in enumerate(line):
+                if cell == '@':
+                    starting_point = (col + 1, row)
+                    break
+    return starting_point
+    
+
+if __name__ == '__main__':
+    robot_loc = find_start()
     if sys.argv[1] == "uniform-cost":
         print("Uniform Cost")
+        uniform_cost(robot_loc)
     elif sys.argv[1] == "depth-first":
         print("Depth First")
     else:
