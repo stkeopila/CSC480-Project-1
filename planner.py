4a0389e325d6010feaee77cd81a5f9b7bd252464
stkeopila <skeopila@calpoly.edu>
2025-04-17 15:01:59 -0700

    Added a file reader to read the sample file

--- planner.py
+++ planner.py
@@ -1,8 +1,15 @@
 import sys
 
-# python3 make_vacuum_world.py 5 7 0.15 3 > sample−5x7.txt
+# python3 make_vacuum_world.py 5 7 0.15 3 > sample-5x7.txt
+
+def uniform_cost():
+    
+    return None
 
 if __name__ == '__main__':
+    with open(sys.argv[2], 'r') as f:
+        columns = print(f.read(1))
+        rows = print(f.read(2))
     if sys.argv[1] == "uniform-cost":
         print("Uniform Cost")
     elif sys.argv[1] == "depth-first":
