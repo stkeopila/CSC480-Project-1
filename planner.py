bc844c4b23537bd04b912932317cd6e57ae27d63
stkeopila <skeopila@calpoly.edu>
2025-04-16 19:43:03 -0700

    added examples to show what is the legend

--- planner.py
+++ planner.py
@@ -1,5 +1,19 @@
 import sys
 
+# 4 (# of columns)
+# 3 (# of rows)
+# _ (empty cell)
+# # (blocked cell)
+# * (dirty cell)
+# @ (robot starting location)
+
+# example
+# 4
+# 3
+# _*__
+# __#*
+# _@*#
+
 if __name__ == '__main__':
     if sys.argv[1] == "uniform-cost":
         print("Uniform Cost")
