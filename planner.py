5b15c855cb8c1c09c1bb73da345c368595590d59
stkeopila <skeopila@calpoly.edu>
2025-04-17 18:25:29 -0700

    created a finding start function, col and row returner

--- planner.py
+++ planner.py
@@ -1,22 +1,27 @@
 import sys
+import heapq
 
 # python3 make_vacuum_world.py 5 7 0.15 3 > sample-5x7.txt
 
 def uniform_cost(rob_loc):
-    print(rob_loc)
+    col = rob_loc[0]
+    row = rob_loc[1]
+    starting_location = rob_loc[2]
+    heap = [(0, starting_location)]
+
     return None
 
 def find_start():
     starting_point = None
     with open(sys.argv[2], 'r') as f:
-        columns = f.read(1)
-        rows = f.read(2)
+        columns = f.read(1).strip()
+        rows = f.read(2).strip()
         for row, line in enumerate(f):
             for col, cell in enumerate(line):
                 if cell == '@':
                     starting_point = (col + 1, row)
                     break
-    return starting_point
+    return (columns, rows, starting_point)
     
 
 if __name__ == '__main__':
