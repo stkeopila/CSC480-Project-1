91b692dd57f07ad1cafd82032a93dd4ae9d3a332
stkeopila <skeopila@calpoly.edu>
2025-04-23 13:59:06 -0700

    Cleaned up code

--- planner.py
+++ planner.py
@@ -1,37 +1,6 @@
 import sys
 import heapq
 
-# python3 make_vacuum_world.py 5 7 0.15 3 > sample-5x7.txt
-# heap = [Cost, robot_location, how many dirty cells cleaned, Vacuum Path, cleaned locations]
-
-# def dfs(rob_loc):
-#     return None
-#     found_all_dirty_cells = False
-#     nodes_expanded = nodesgenerated = 0
-#     col, row, robot, dirty_cells, grid = rob_loc[0], rob_loc[1], rob_loc[2], rob_loc[3], rob_loc[4]
-#     heap = [(0, robot, 0, [], [])]
-#     visited = set([(robot, frozenset())])
-#     moves = [(-1, 0, "N"),(1, 0, "S"),(0, -1, "W"),(0, 1, "E")]
-#     while len(dirty_cells) != heap[0][2]:
-#         for r, c, dir in moves:
-#             rob_row, rob,col = robot[0] + r, robot[1] + c
-#             if 0 <= rob_row < row and 0 <= rob_col < col and grid[rob_row][rob_col] != '#':
-#                 nodes_generated += 1
-#                 nodes_expanded += 1
-#                 new_pos = (rob_row, rob_col)
-#                 new_cleaned = list(heap[0][4])
-#                 if grid[rob_row][rob_col] == '*' and new_pos not in heap[0][4]:
-
-# need to store previous robot location so i dont research the area
-# return back a int value when found a dirty cell
-# 
-
-# heap = [Cost, robot_location, how many dirty cells cleaned, Vacuum Path, cleaned locations]
-# stack = [robot location, path, visited states, how many dirty cells cleaned]
-
-# return (columns, rows, starting_point, dirty_cells, grid)
-# dfs(robot_loc, visited, path, moves)
-
 def dfs(rob_loc):
     col, row, robot, dirty_cells, grid = rob_loc[0], rob_loc[1], rob_loc[2], rob_loc[3], rob_loc[4]
     path = []
@@ -69,18 +38,6 @@ def dfs(rob_loc):
         print(dir)
     print(F"{nodes_generated} nodes generated\n{nodes_expanded} nodes expanded")
     return path
-    
-    
-
-
-# def dfs(rob_loc):
-#     found_all_dirty_cells = False
-#     nodes_expanded = nodesgenerated = 0
-#     col, row, robot, dirty_cells, grid = rob_loc[0], rob_loc[1], rob_loc[2], rob_loc[3], rob_loc[4]
-#     moves = [(-1, 0, "N"),(1, 0, "S"),(0, -1, "W"),(0, 1, "E")]
-#     dfs_helper(rob_loc)
-
-
 
 def uniform_cost(rob_loc):
     found_all_dirty_cells = False
@@ -114,7 +71,6 @@ def uniform_cost(rob_loc):
         nodes_expanded += 1
         heapq.heappop(heap)
         robot = heap[0][1]
-    # print(f"Cost: {heap[0][0]} Found:", heap[0][3])
     for dir in heap[0][3]:
         print(dir)
     print(F"{nodes_generated} nodes generated\n{nodes_expanded} nodes expanded")
